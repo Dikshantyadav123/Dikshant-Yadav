60bf29eb002307450017752ded0a1ec1b8d4368e
DIKSHANT YADAV <dikshantyadav2023@gmail.com>
2024-02-25 21:47:48 +0530

    update js g sap

--- javascript/one.js
+++ javascript/one.js
@@ -2,6 +2,9 @@
 let tl = gsap.timeline()
 
 // add the tweens to the timeline - Note we're using tl.to not gsap.to
-tl.to(".green", { x: 600, duration: 2 });
-tl.to(".purple", { x: 600, duration: 1 });
-tl.to(".orange", { x: 600, duration: 1 });
+tl.from(".head", { x: "600vw", duration: 2 });
+tl.from(".box_A", { x: "600vw", duration: 1 });
+tl.from(".box1", { x: "600vw", duration: 1 });
+tl.from(".box2", { x: "600vw", duration: 2 });
+tl.from(".box3", { x: "600vw", duration: 1 });
+tl.from(".box4", { x: "600vw", duration: 1 });
